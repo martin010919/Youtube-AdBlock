68a2320f1a69092d264fd1d27cdbaa4bc18b601d
Mattgwat <70171040+martin010919@users.noreply.github.com>
2023-10-28 10:35:03 +0000

    Update adblocker.js
    step 2

--- adblocker.js
+++ adblocker.js
@@ -13,6 +13,9 @@
 (function() {
     //strict rule
     `use strict`;
+
+    //YelloNello src start:
+    //-----------------------------------------
     // Any class the blocker uses
     const blockerClass = 'ytd-enforcement-message-view-model';
     // Any class on the broken video
@@ -67,6 +70,258 @@
         }
     }
 
+    //recheck dropdown
+    function dropdownTracker() {
+        var dropdown = document.getElementById("dropdown");
+
+        if (dropdown) {
+            dropdown.addEventListener('change', function () {
+                var newValue = dropdown.value;
+
+                // Check if the value has actually changed
+                if (newValue !== previousDropdownValue) {
+                    newDomain = domainList[newValue];
+                    console.log("Selection Changed: " + newDomain);
+                    reloadFrame();
+
+                    // Update the previousValue variable
+                    previousDropdownValue = newValue;
+                }
+            });
+        }
+    }
+
+    //iframe reload
+    function reloadFrame() {
+        replaceVideo();
+        addDomainToURLs();
+
+        console.log("clicked");
+    }
+
+    //checklist
+    function appendingFrame(isSet) {
+        if (isSet == true) {
+            isChangingFrame = true;
+        } else {
+            isChangingFrame = false;
+        }
+        console.log("ChangingFrames: " + changingFrame)
+    }
+
+    function restRead() {
+        null;
+    }
+    
+    //action: locate class name
+    function removeElementsByClassName(removeClass) {
+        console.log("Removing [removeElementsByClassName]: " + removeClass);
+        const elements = document.querySelectorAll('.' + removeClass);
+        elements.forEach(element => {
+            element.remove();
+        });
+    }
+
+    //checks matching
+    function checkText(string, text) {
+        console.log("Checking string [checkText]")
+        return string.includes(text);
+    }
+
+    //replace youtube with youtube/embed 
+    function getNewURL(newDomain) {
+        console.log("New URL [newDomain]: " + newDomain);
+        const currentURL = window.location.href;
+        if (currentURL.includes(youtubeURL)) {
+            const newURL = currentURL.replace(youtubeURL, newDomain);
+            return newURL;
+        }
+    }
+
+    //safetyfix: makes all links yt.com
+    function addDomainToURLs() {
+        console.log("Adding [addDomainToURLs]");
+        const links = document.querySelectorAll('a');
+
+        links.forEach(link => {
+            let href = link.getAttribute('href');
+            if (href && !href.startsWith('http') && !href.startsWith('www')) {
+                href = 'https://www.' + youtubeURL + href;
+
+                link.setAttribute('href', href);
+            }
+        });
+    }
+
+    //more url fixing
+    function fixURL(URL) {
+        const isURL = checkText(URL, youtubeURL);
+        const isPlaylist = checkText(URL, "&list=");
+        const isTimestamp = checkText(URL, "&t=");
+
+        console.log("isURL:" + isURL + " isPlaylist:" + isPlaylist + " isTimestamp:" + isTimestamp);
+        console.log("URL [fixURL]:" + URL);
+
+        if (isURL && !isPlaylist) {
+            URL = URL.replace("watch?v=", "");
+            console.log("Is Playlist [fixURL]: " + URL);
+        }
+        if (isURL && isTimestamp){
+            URL = URL.split("&t=")[0];
+            console.log("URL Split [fixURL]: " + URL);
+        }
+        return URL;
+    }
+
+    //switch
+    function replaceVideo() {
+        if (!isSubChange) {
+            console.log("replacing [replaceVideo]");
+            removeElementsByClassName(blockerClass);
+            createJFrame(ogVideoClass);
+            isSubChange = true;
+
+            return;
+        }
+        if (isSubChange) {
+            console.log("replacing subclick [replaceVideo]");
+            removeOgIframe();
+            createJFrame(tempReplaceClass);
+            console.log("In with the new [replaceVideo]");
+        }
+
+        isBlocked = false;
+    }
+
+    //create frame.
+    function createJFrame(classToOverturn) {
+        var newURL = getNewURL(newDomain);
+        const elements = document.querySelectorAll("." + classToOverturn);
+        console.log("newURL Beginning [createJFrame]: " + newURL);
+
+        newURL = fixURL(newURL);
+
+        elements.forEach(element => {
+            const iframe = document.createElement('iframe');
+            iframe.width = '100%';
+            iframe.height = '100%';
+            iframe.src = newURL;
+            iframe.allow = 'accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+            // autoplay                   ^ (autoplay removed after bug; also never worked anyhow)
+            iframe.allowFullscreen = true;
+            iframe.zIndex = '9999';
+
+            // Replace the existing element with the custom URL
+            element.parentNode.replaceChild(iframe, element);
+            console.log("Modified URL:", newURL);
+        });
+    }
+
+
+    //replace vid
+    function removeOgIframe() {
+        const iframes = document.querySelectorAll('iframe');
+        console.log("removing jFrame [removeOgIframe]");
+        iframes.forEach(iframe => {
+            const paragraph = document.createElement('p');
+            paragraph.className = tempReplaceClass;
+            iframe.parentNode.insertBefore(paragraph, iframe);
+            iframe.remove();
+            console.log("Out with the old [removeOgIframe]");
+        });
+    }
+
+    //css buttons
+    var css = `
+    .btn-style {
+        position: relative;
+        display: inline-block;
+        padding: 9px;
+        height: 40px;
+        color: white;
+        background-color: transparent;
+        box-shadow: none;
+        text-shadow: none;
+        border: 1px solid white;
+        border-radius: 0px;
+        z-index: 9999;
+        opacity: 100%;
+        transition: transform 0.3s ease;
+        user-select: none;
+    }
+    .btn-style:hover {
+        opacity: 80%;
+    }
+    .main-btn {
+        margin-right: 16px;
+        transition: transform 0.1s ease;
+    }
+    .main-btn:active {
+        border: 1px solid transparent !important;
+        transform: scale(0.9);
+    }
+    .dropdown-content {
+        position: relative;
+        background-color: black;
+    }
+    .custom-container {
+        border: none;
+        display: inline-block;
+        margin-right: 16px;
+        margin-left: 16px;
+        transition: transform 0.3s ease;
+
+    }
+    `;
+    // Create Container
+    var customContainer = document.createElement("div");
+    customContainer.classList.add("custom-container");
+
+    // Create Button
+    var reloadButton = document.createElement('button');
+    reloadButton.textContent = 'Reload Frame';
+    reloadButton.classList.add("btn-style", "main-btn");
+
+    // Create Dropdown Menu
+    var dropdownButton = document.createElement("select");
+    dropdownButton.id = "dropdown";
+    dropdownButton.classList.add("btn-style");
+    dropdownButton.innerHTML = `
+        <option class="dropdown-content" value="0">YouTube Embed</option>
+        <option class="dropdown-content" value="1">YouT-ube [Fixed?]</option>
+        Broke <option class="dropdown-content" value="2">NSFW YouTube [Broken!]</option>
+    `;
+    // Add items to Container
+    customContainer.appendChild(reloadButton);
+    customContainer.appendChild(dropdownButton);
+ 
+    // Append CSS to page
+    var style = document.createElement("style");
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+ 
+    // Find ID Location
+    var exsistingParent = document.getElementById("end");
+ 
+    // Add Container to Page
+    var exsistingParent = document.getElementById("end");
+    exsistingParent.insertBefore(customContainer, exsistingParent.firstChild);
+
+    // -------------- Active Listeners -------------- //
+
+    // Listen for reload BTN click
+    reloadButton.addEventListener('click', reloadFrame);
+
+    // Run every second to check for updates on page (Will not ping any server till a new page is clicked)
+    const classCheckInterval = setInterval(checkClass, 1000);
+    document.addEventListener('click', checkClass, 1000);
+
+    //YelloNello src end
+    //------------------------------------------
+
+
+    //iamfugui src start
+    //-------------------------------------------
     const cssSeletorArr = [
         `#masthead-ad`,
         `ytd-rich-item-renderer.style-scope.ytd-rich-grid-row #content:has(.ytd-display-ad-renderer)`,
@@ -84,8 +339,8 @@
     window.dev=true;
 
     /**
-    * @param {Date} time 标准时间
-    * @param {String} format 格式
+    * @param {Date} time
+    * @param {String} format
     * @return {String}
     */
     function moment(time, format = `YYYY-MM-DD HH:mm:ss`) {
